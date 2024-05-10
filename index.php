6a87500ba5b0b92ef56526bbdefecbed61b0d6cb
Tristan Plaus <93706640+EunjiTan@users.noreply.github.com>
2024-05-10 12:33:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<div class="container">
+<div class="row">
+<div class="col-md-10 col-md-offset-1">
+<div class="title text-center">
+<h1>Tristan S. Plaus</h1>
+<h3>Software Engineer</h3>
+<h4><a href="portfolio1.css">PlausPortfolio.com</a></h4>
+<hr />
+<ul class="list-inline">
+<li><i class="devicon-laravel-plain colored"></i></li>
+<li><i class="devicon-javascript-plain colored"></i></li>
+<li><i class="devicon-html5-plain colored"></i></li>
+<li><i class="devicon-angularjs-plain colored"></i></li>
+<li><i class="devicon-linux-plain"></i></li>
+</ul>
+</div>
+
+<!-- summary section -->
+<div class="summary">
+<h2 class="text-center">Summary</h2>
+<p>A Learning Software Engineer with over 2 years of experience developing web applications. Looking for challenging roles in web and mobile industry to enhance my skills and provide my services to add value to the products of the organization. Highly motivated self learner with a passion for learning and keeping informed of the latest in technology.</p>
+
+<div class="row">
+<div class="col-md-3 col-sm-3">
+<p>Proficient in programming languages: Java, Python, JavaScript</p>
+<p>Strong understanding of data structures and algorithms</p>
+<p>Quality Assurance </p>
+</div>
+<div class="col-md-3 col-sm-3">
+<p>Experience with version control systems such as Git</p>
+<p>Knowledge of relational and non-relational databases: MySQL, MongoDB</p>
+<p>Excellent problem-solving and analytical skills</p>
+</div>
+<div class="col-md-3 col-sm-3">
+<p>Agile Development</p>
+<p>Strong communication and teamwork abilities</p>
+<p>Frontend Development</p>
+</div>
+<div class="col-md-3 col-sm-3">
+<p>Code Review</p>
+<p>Peer Review</p>
+<p>Software Engineer</p>
+</div>
+</div>
+
+</div>
+
+
+<!-- work experience section -->
+<div class="work-experience">
+<h2 class="text-center">Work Experience</h2>
+<div class="row experience-title">
+<div class="col-md-9">
+<h3>Collaborated with senior engineers to develop and maintain web applications using Java and Php</h3>
+<h4>Participated in code reviews and provided constructive feedback to team members</h4>
+<h5>Assisted in designing and optimizing database schemas for improved performance</h5>
+</div>
+<div class="col-md-3">
+<h3>May 2024 - Present</h3>
+</div>
+</div>
+<div class="row experience-summary">
+<div class="col-md-12">
+<p>Far Eastern Institute of Technology - Conducted unit testing and contributed to the continuous integration process</p>
+</div>
+</div>
+
+<!-- Corasent -->
+<div class="row experience-title">
+<div class="col-md-9">
+<h3>E-Commerce Website</h3>
+<h4>Data Visualization Dashboard</h4>
+<h5>Some project</h5>
+</div>
+<div class="col-md-3">
+<h3>Jan 2022 - Jun 2023</h3>
+</div>
+</div>
+<div class="row experience-summary">
+<div class="col-md-12">
+<p>Created a data visualization dashboard using Python's Dash framework, Designed interactive charts and graphs to represent complex data sets</p>
+<ul>
+<li>Integrated with SQL database to fetch real-time data and deployed the dashboard on Heroku.</li>
+<li>WordPress expert - Configuration, maintenance and custom development.</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
